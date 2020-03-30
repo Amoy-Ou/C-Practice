ff15c7b84d12f4fe6db03dd36409ba773b769eec
Amoy-Ou <2506745617@qq.com>
2020-03-30 15:56:38 +0800

    AgeToMouth

---
+++ AgeToMon.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    cout << "Enter you age:";
+    int age;
+    cin >> age;
+    cout << "You have spend " << age * 12 << " months" << endl;
+    return 0;
+}
